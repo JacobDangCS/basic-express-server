945c8d140b15741ba0b18e4ca65a4f4690df217f
JacobDangCS50 <jacobohdang@gmail.com>
2022-11-30 17:25:26 +0000

    adjust validator

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -3,22 +3,18 @@
 let validator = require('../src/middleware/validator');
 
 describe('Validator middleware', () => {
+    let req = {query: {name: Jacob}};
+    let res = {};
     let next = jest.fn(); //mocks the "next" method
-
+    
     it('validates query as expected', () => {
-        const req = {
-            query:
-                { name: 'Jacob', },
-        }
-        validator(req, {}, next);
+        validator(req, res, next);
         expect(next).toHaveBeenCalledWith();
     });
 
     it('invalid query', () => {
-        const req = {
-            query: {},
-        };
-        validator(req, {}, next);
+        let req = {query: {}};
+        validator(req, res, next);
         expect(next).toHaveBeenCalledWith('name required. no name provided')
     });
 });
--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const validator = (req, res, next) => {
-    if (req.query.name) next();
+    if (req.query.name){
+        next();
+    }
     else next ('name required. no name provided');
 };
 
